af7f637b8cebda6b8e4f700cc3c5e5d234596bd9
naseeb <naseebnaushad46@gmail.com>
2025-09-01 17:58:15 +0530

    Updated the project

--- app/src/main/java/tech/unmashed/musiclauncher/MainActivity.java
+++ app/src/main/java/tech/unmashed/musiclauncher/MainActivity.java
@@ -1,17 +1,17 @@
 package tech.unmashed.musiclauncher;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity; // ✅ AndroidX import
 
 public class MainActivity extends AppCompatActivity {
 
     Intent launchIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         this.launchIntent = getPackageManager().getLaunchIntentForPackage(getString(R.string.target_app));
         if (this.launchIntent != null) {
